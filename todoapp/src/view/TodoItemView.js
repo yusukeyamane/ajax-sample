ebd0c7b0ab9eef9c514f12582e76196ef99ecfd2
Yusuke Yamane <yusukeyamane01@gmail.com>
2019-02-14 11:28:29 +0900

    Create TodoItemView.js

---
+++ todoapp/src/view/TodoItemView.js
@@ -0,0 +1,37 @@
+import { element } from "./html-util.js";
+
+export class TodoItemView {
+   /**
+   * `todoItem`に対応するTodoアイテムのHTML要素を作成して返す
+   * @param {TodoItemModel} todoItem
+   * @param {function({id:string, completed: boolean})} onUpdateTodo チェックボックスの更新イベントリスナー
+   * @param {function({id:string)}} onDeleteTodo 削除ボタンのクリックイベントリスナー
+   * @returns {Element}
+   */
+
+  createElement(todoItem, {onUpdateTodo, onDeleteTodo }) {
+    const todoItemElement = todoItem.completed
+      ? element`<li><input type="checkbox" class="checkbox" checked>
+                  <s>${todoItem.title}</s>
+                  <button class="delete">x</button>
+                </input></li>`
+      : element`<li><input type="checkbox" class="checkbox">
+                  ${todoItem.title}
+                  <button class="delete">x</button>
+                </input></li>`;
+    const inputCheckBoxElement = todoItemElement.querySelector(".checkbox");
+    inputCheckBoxElement.addEventListener("change", () => {
+      onUpdateTodo({
+        id: todoItem.id,
+        completed: !todoItem.completed
+      });
+    });
+    const deleteButtonElement = todoItemElement.querySelector(".delete");
+    deleteButtonElement.addEventListener("click", ()=> {
+      onDeleteTodo({
+        id: todoItem.id
+      });
+    });
+    return todoItemElement;
+  }
+}
